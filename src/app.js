7c088a9ae6e89f6f16d4e28e62fe53895740d6ec
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-11-25 08:27:40 +0100

    Add Morgan DotEnv Mongoose deps & Change dev script

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+dotenv.config()
 
 import { router } from './routes/index.routes.js'
 
@@ -10,6 +13,7 @@ app.set('port', 3000)
 // Middlewares
 app.use(express.json())
 app.use(express.static('public'))
+app.use(morgan('dev'))
 
 // Routes
 app.use('/api', router)
