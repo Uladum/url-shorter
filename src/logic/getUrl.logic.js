3b9604e10a540873a47d5e4aeea4d377de3ff183
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-11-29 11:29:07 +0100

    Add Express validator config

--- src/logic/getUrl.logic.js
+++ src/logic/getUrl.logic.js
@@ -1,7 +1,5 @@
 export const getUrl = async url => {
   try {
-    if (!url) return { status: 404, data: { msg: 'URL not found' } }
-
     console.log(url)
 
     return { status: 200, data: { url } }
--- src/routes/getUrl.routes.js
+++ src/routes/getUrl.routes.js
@@ -1,8 +1,18 @@
 import { Router } from 'express'
+import { check } from 'express-validator'
+import validateResults from '../middlewares/validateResults.middleware.js'
 import { getUrl } from '../controllers/getUrl.controller.js'
 
 const router = Router()
 
-router.get('/', getUrl)
+router.get(
+  '/',
+  [
+    check('url', 'URL is required.').not().isEmpty(),
+    check('url', 'URL not allowed').isURL(),
+    validateResults
+  ],
+  getUrl
+)
 
 export default router
