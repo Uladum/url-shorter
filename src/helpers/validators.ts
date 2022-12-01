90d427e5c038a38eba4c1a12c4d4d46aeb3b637d
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-12-01 13:46:34 +0100

    Delete existent URL

--- src/helpers/validators.ts
+++ src/helpers/validators.ts
@@ -1,13 +1,5 @@
 import Code from '../models/url.js'
 
-export const existentUrl = async (url: string) => {
-  const existentUrl = await Code.findOne({ url })
-  if (existentUrl)
-    throw new Error(
-      `URL ${url} alredy exists on BD with code ${existentUrl.code}`
-    )
-}
-
 export const nonexistentCode = async (code: string) => {
   const existentCode = await Code.findOne({ code })
   if (existentCode === null) throw new Error(`Code ${code} not found`)
